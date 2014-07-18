4b57853eb91adf67903e6f05589b6214c9bb6ae0
Vincent Lin <vincent@spoonrocket.com>
2014-07-18 19:05:05 +0800

    revise notify mailer

--- app/models/mention_mailer.rb
+++ app/models/mention_mailer.rb
@@ -4,11 +4,11 @@ class MentionMailer < ActionMailer::Base
   def self.default_url_options
     Mailer.default_url_options
   end
-  
-  
+
+
   def notify_mentioning(issue, journal, user)
     @issue = issue
     @journal = journal
-    mail(to: user.mail, subject: "You were mentioned in a note")
+    mail(to: user.mail, subject: "You were mentioned in a issue.")
   end
 end
