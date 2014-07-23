a12b897c8d7e355cd3e52b97c2cb2fe25cdcdd42
Vincent Lin <vincent@spoonrocket.com>
2014-07-23 15:13:31 +0800

    revised mention mailer html

--- app/models/mention_mailer.rb
+++ app/models/mention_mailer.rb
@@ -7,6 +7,7 @@ class MentionMailer < ActionMailer::Base
 
   def notify_mentioning(issue, journal, user)
     @issue = issue
+    @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue, :anchor => "change-#{journal.id}")
     @journal = journal
     subject = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] You were mentioned. #{issue.subject}"
     mail(to: user.mail, subject: subject)
