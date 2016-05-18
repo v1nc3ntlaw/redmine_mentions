803bd5062233ae05b1d3c0444c9d265981c641b2
Vincent Lin <vincent@spoonrocket.com>
2016-05-19 02:24:06 +0800

    fix in newer version redmine

--- lib/redmine_mentions/journal_patch.rb
+++ lib/redmine_mentions/journal_patch.rb
@@ -3,12 +3,12 @@ module RedmineMentions
     def self.included(base)
       base.class_eval do
         after_create :send_mail
-        
+
         def send_mail
           if self.journalized.is_a?(Issue) && self.notes.present?
             issue = self.journalized
             project=self.journalized.project
-            users=project.users.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
+            users=project.users.to_a.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
             users_regex=users.collect{|u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.login}"}.join('|')
             regex_for_email = '\B('+users_regex+')'
             regex = Regexp.new(regex_for_email)
